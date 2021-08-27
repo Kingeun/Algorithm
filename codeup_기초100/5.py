71ebec9dea38e6f0c94d67954d42f6e3bd8601ee
Kingeun:) <85938160+Kingeun@users.noreply.github.com>
2021-08-27 23:16:51 +0900

    Create 5.py

---
+++ codeup_기초100/5.py
@@ -0,0 +1,19 @@
+#6077
+n=int(input())
+cnt=0
+for i in range(1,n+1):
+    if i%2==0:
+        cnt+=i
+print(cnt)
+
+
+#6078
+
+while True:
+    n=input()
+    print(n)
+    if n=='q':
+        break
+        
+      
+ 
