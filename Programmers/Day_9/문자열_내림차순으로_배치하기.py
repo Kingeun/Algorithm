c1e1e2e633ee6c12e5db87a310b6a8c2d1a51d00
Kingeun:) <85938160+Kingeun@users.noreply.github.com>
2021-09-20 03:14:47 +0900

    Create 문자열_내림차순으로_배치하기.py

---
+++ Programmers/Day_9/문자열_내림차순으로_배치하기.py
@@ -0,0 +1,12 @@
+def solution(s):
+    s=sorted(s,reverse=True)
+    answer="".join(s)
+    return answer
+
+'''
+#다른 사람 풀이
+def solution(s): 
+    answer = "".join(sorted(s)[::-1]) 
+    return answer
+
+'''
